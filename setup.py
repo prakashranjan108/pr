c65905cf890d4115e7ee946e9e6b1a3c02fd4337
prakashranjan108 <prakashranjansatyarthi024@gmail.com>
2025-03-18 16:02:26 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prakash Ranjan Satyarthi",
+    author_email="prakashranjansatyarthi024@gmail.com",
+    packages=find_packages()
+)
